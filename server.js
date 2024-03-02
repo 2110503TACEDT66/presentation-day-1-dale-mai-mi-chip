38e7f0fc93684f6a734716aa7435dc45c5f6af62
NaMMly YuMMy <andavarinthorn1409@gmail.com>
2024-03-02 22:18:32 +0700

    change to my port

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const swaggerOptions = {
         },
         servers : [
             {
-                url : 'http://localhost:5010/api/v1'
+                url : 'http://localhost:5000/api/v1'
             }
         ],
     },
@@ -66,7 +66,7 @@ app.use(cookieParser());
 app.use(mongoSanitize());
 
 
-const PORT = process.env.PORT || 5010;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 
 process.on('unhandledRejection', (err, promise) => {
