c1f4ace95843c4c7291c72a7fb74ca000f854d08
NaMMly YuMMy <andavarinthorn1409@gmail.com>
2024-03-03 16:37:38 +0700

    check for get put and post in postman

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const {register, login, getMe, logout} = require('../controllers/auth'); //Get function from /controllers/auth
+const {register, login, getMe, logout, getAllUsers} = require('../controllers/auth'); //Get function from /controllers/auth
 
 const router = express.Router(); //Get request from which method 
 
-const  {protect} = require('../middleware/auth')
+const {protect, authorize} = require('../middleware/auth');
 
 router.post('/register', register); //If it is from post method and from /register path then do register funciton from above
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/logout', logout);
+router.route('/all').get(protect, authorize('admin'), getAllUsers);
 
 module.exports = router;
